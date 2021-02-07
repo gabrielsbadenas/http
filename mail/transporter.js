1d6595ba840a7fd70a5d01607291172363eee328
Gabriel <66344280+gabrielsbadenas@users.noreply.github.com>
2021-02-07 02:59:10 -0300

    Update transporter.js

--- mail/transporter.js
+++ mail/transporter.js
@@ -27,28 +27,31 @@ var mail = {
       })
   }
 }
-
+//tengo que pasar por constructor el json
+//lo que va con "" son los strings y los nombres de variables
+//boolean y numeros van sin ""
 class Mail{
-  constructor(config){
-    this.config = config
-    setTransporter(config)
-
-  }
-  setTransporter(config){
-    this.transporter = nodemailer.createTransport(config)
-  }
-  send(mensaje){
-    this.transporter.sendMail(mensaje,mensaje, (error, info) => {
-        if(error) {
-            res.status(500).send(error.message)
-        } else {
-            console.log("Email enviado")
-            res.status(200).jsonp(req.body)
-        }
-    })
-  }
+	constructor(config){
+		this.transporter = nodemailer.createTransport(config)
+	}
+	send(mensaje){
+		this.transporter.sendMail(mensaje)
+	}
 }
 
-mail.send("hi","xd")
+
+//mail.send("hi","xd")
+class Data{
+	constructor(host, post, secure, _user, _pass){
+		this.host
+		this.post
+		this.secure
+		this.auth = {
+			user: _user,
+			pass: _pass
+		}
+	}
+}
+const m = new Mail()
 
 //module.exports = transporter
