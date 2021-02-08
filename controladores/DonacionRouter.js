621b3d6a4386c13a8528120308328f79c0c63486
gabriel <gabrielsbadenas@gmail.com>
2021-02-08 01:43:37 -0300

    const,

--- controladores/DonacionRouter.js
+++ controladores/DonacionRouter.js
@@ -1,18 +1,19 @@
-const DonacionDAO = require('../daos/DonacionDAO.js')
-const Mailer = require('../mail/mailer')
-const efectuarDonacion = require('../casosDeUso/efectuarDonacion')
-const listarDonaciones = require('../casosDeUso/listarDonaciones')
-const options = require('../config/options')
+const DonacionDAO = require('../daos/DonacionDAO.js'),
+//Mailer = require('../mail/mailer'),
+efectuarDonacion = require('../casosDeUso/efectuarDonacion'),
+listarDonaciones = require('../casosDeUso/listarDonaciones'),
+options = require('../config/options'),
+bodyParser = require('body-parser'),
+urlencodedParser = bodyParser.urlencoded({ extended: false }),
+express = require('express'),
+rtr = express.Router()
 
-const bodyParser = require('body-parser')
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
-
-const express = require('express')
-const rtr = express.Router()
 rtr.use(bodyParser.json())
-let mailer = new Mailer(options.mail.host,options.mail.auth.user, options.mail.auth.pass)
+
+//let mailer = new Mailer(options.mail.host,options.mail.auth.user, options.mail.auth.pass)
 let dao = new DonacionDAO()
 efectuarDonacion(mailer, dao, 14888, "gabriel")
+
 rtr.get('/', function (req, res) {
 	res.send(dao.getAll())
 })
@@ -20,6 +21,7 @@ rtr.get('/', function (req, res) {
 //todo arreglar el post
 rtr.post('/', urlencodedParser, function(req, res) {
 	//console.log(req.body)
+	efectuarDonacion(mailer, dao, req.bod.monto, req.body.nombre)
 	res.end('ok')
 	//efectuarDonacion(mailer, dao, req.body.monto, req.body.nombre)
 	//hacer lo que dice la correccion de
